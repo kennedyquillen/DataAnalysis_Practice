6723a668d266e273ce7f2c52b9adeecac339637c
kennedyquillen <quillen@usf.edu>
2025-08-21 14:08:09 -0400

    Folders created

--- GreatBritishBakeoff_Analysis.R
+++ GreatBritishBakeoff_Analysis.R
@@ -1,1 +1,13 @@
-push
+# Great British Bake-Off Analysis (By: Kennedy Quillen, August 2025)
+# ----------------------------------------------------------------------------
+
+## Organize the project into folders and sub-folders 
+
+# Download the required package
+install.packages('fs')
+
+# Create the project folder structure
+fs::dir_create(c("R", "data", "outputs/figures", 
+                 "outputs/tables", "outputs/data", 
+                 "outputs/models", "resources"))
+
