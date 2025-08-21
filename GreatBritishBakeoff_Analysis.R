4c50c76ad35e66ee3760db0495c4679f01af515e
kennedyquillen <quillen@usf.edu>
2025-08-21 14:26:42 -0400

    imported data

--- GreatBritishBakeoff_Analysis.R
+++ GreatBritishBakeoff_Analysis.R
@@ -1,7 +1,6 @@
-# Great British Bake-Off Analysis (By: Kennedy Quillen, August 2025)
-# ----------------------------------------------------------------------------
+# Great British Bake-Off Analysis (By: Kennedy Quillen, August 2025) ----
 
-## Organize the project into folders and sub-folders 
+## Organize the project into folders and sub-folders ----
 
 # Download the required package
 install.packages('fs')
@@ -10,4 +9,11 @@ install.packages('fs')
 fs::dir_create(c("R", "data", "outputs/figures", 
                  "outputs/tables", "outputs/data", 
                  "outputs/models", "resources"))
+
+## Import the data ----
+bakers <- read.csv("data/Bakers.csv")
+challenges <- read.csv("data/ChallengeBakes.csv")
+episodes <- read.csv("data/Episodes.csv")
+outcomes <- read.csv("data/Outcomes.csv")
+seasons <- read.csv("data/Seasons.csv")
 
